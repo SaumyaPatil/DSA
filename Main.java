f6e90c796cb5c10069273a515f72dbe41fc8732e
SaumyaPatil <saupatil10dec@gmail.com>
2025-07-06 16:13:44 +0530

    Learning DSA

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main{
+    public static void main(String[] args) {
+        int[] rnos = new int[5];
+
+        //or directly
+        int[] rnos2 = {1, 2, 3, 4, 5};
+
+        //declaration of array. ros is getting defined in the stack
+        int[] ros;  
+
+        //initialisation: actually here object is being created in the memory (heap)
+        ros = new int[5];
+    }
+}
+
+// In C / C++, there is continuous memory allocation to the elements of the array
+// In java theres no concept of pointers, it totally depends on JVM, 
+// whether this is going to be continuous or not.
+
+// 1. Array objects are in heap. All the objects are stored in heap section.
+// 2. In JLS (Java Language Specification) its mentioned that heap objects are not continuous. 
+// 3. Heap is the runtime data area where the memory of all the classes or instances are allocated.
+
+
+// Hence array objects in java may not be continuous.
+// Depends on JVM.
+// 'new' is used to create an object.
+
